fa0b661726b60ece09d0c72516636137f833f9b3
rudalsss <linda284@naver.com>
2025-10-03 20:21:01 +0900

    refactor: JWT filter, JWT util 수정및 테스트 컨트롤러 수정

--- src/main/java/com/financedoc/gateway_service/JwtTestController.java
+++ src/main/java/com/financedoc/gateway_service/JwtTestController.java
@@ -1,19 +1,25 @@
 package com.financedoc.gateway_service;
 
 import com.financedoc.gateway_service.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
+@RequiredArgsConstructor
 public class JwtTestController {
     private final JwtUtil jwtUtil;
 
-    public JwtTestController(JwtUtil jwtUtil) {
-        this.jwtUtil = jwtUtil;
-    }
-
     @GetMapping("/test-token")
     public String generateTestToken() {
-        return jwtUtil.createAccess("test@example.com");
+        // 테스트용 userId
+        String testUserId = "testUserId";
+
+        // 필요하면 claims를 넣을 수도 있음예시
+        Map<String, Object> claims = Map.of("nickname", "TestUser's Nickname");
+
+        return jwtUtil.createAccessToken(testUserId, claims);
     }
 }
